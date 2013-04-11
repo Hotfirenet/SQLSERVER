d6a630a32010bc0b701b96a0569404f6784f8e84
Johan .V <hotfirenet@gmail.com>
2013-04-11 15:02:17 +0300

    Create anneeBissextile.sql

---
+++ requetes/anneeBissextile.sql
@@ -0,0 +1,14 @@
+CREATE FUNCTION anneeBissextile (@Annee AS int) 
+RETURNS bit
+AS
+BEGIN
+    Declare @Date29Fev AS Datetime
+    Declare @Result AS bit
+    SET @Date29Fev  = DATEADD(DAY, -1, Cast('01/03/'+Cast(@Annee AS varchar(4)) AS datetime))
+    IF Day(@Date29Fev )=29    
+        SET @Result =1
+    ELSE
+        SET @Result = 0
+    return @Result
+END
+
