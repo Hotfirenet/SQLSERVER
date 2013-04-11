98dd0a77156a58f6df33f55021b928eb7efe8295
Johan .V <hotfirenet@gmail.com>
2013-04-11 15:03:34 +0300

    Ajout de l'entete

--- requetes/anneeBissextile.sql
+++ requetes/anneeBissextile.sql
@@ -1,3 +1,9 @@
+-- =============================================
+-- Author:        Johan .V
+-- Create date: 11 mars 2013
+-- Description:   Ce script permet de calculer et de définir les jours fériés 
+-- =============================================
+
 CREATE FUNCTION anneeBissextile (@Annee AS int) 
 RETURNS bit
 AS
@@ -6,7 +12,7 @@ BEGIN
     Declare @Result AS bit
     SET @Date29Fev  = DATEADD(DAY, -1, Cast('01/03/'+Cast(@Annee AS varchar(4)) AS datetime))
     IF Day(@Date29Fev )=29    
-        SET @Result =1
+        SET @Result = 1
     ELSE
         SET @Result = 0
     return @Result
